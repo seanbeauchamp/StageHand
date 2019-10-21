dff9b654fed505bf2cd0bfb28aadcca2507fce6a
Sean Beauchamp <sean.marc.beauchamp@gmail.com>
2019-10-20 21:46:03 -0300

    New properties set for the Buzzsaw Prefab's script.

--- Assets/Scripts/Buzzsaw.cs
+++ Assets/Scripts/Buzzsaw.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Buzzsaw : MonoBehaviour
 {
+    Dictionary<string, Vector2> directions = new Dictionary<string, Vector2>();
+    [SerializeField]
+    string currentDirection;
+    [SerializeField]
+    bool goingRight;
+    Vector2 origin;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        directions.Add("up", new Vector2(0, 1));
+        directions.Add("down", new Vector2(0, -1));
+        directions.Add("left", new Vector2(-1, 0));
+        directions.Add("right", new Vector2(1, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        origin = transform.GetComponent<Renderer>().bounds.center;
+
+
+    }
+
+    void checkIfStillOnPlatform()
+    {
+
     }
 }
