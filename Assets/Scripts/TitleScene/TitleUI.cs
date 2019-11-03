b039e70f12afeb16fc7c9d5613766d8983e461c8
Sean Beauchamp <sean.marc.beauchamp@gmail.com>
2019-11-03 12:15:32 -0400

    Hooked up Title Screen interactivity.

---
+++ Assets/Scripts/TitleScene/TitleUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUI : MonoBehaviour
+{
+    public Text start;
+    public Text credits;
+    public Text exit;
+
+    float[] positions = new float[3];
+    float cursorOrigin;
+    
+    public Image cursor;
+    int currentCursorPos = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions[0] = start.transform.position.y;
+        positions[1] = credits.transform.position.y;
+        positions[2] = exit.transform.position.y;
+
+        cursor.transform.position = new Vector3(cursor.transform.position.x, positions[currentCursorPos], cursor.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkForInput();
+    }
+
+    void checkForInput()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            currentCursorPos = (currentCursorPos >= positions.Length - 1 ? 0 : currentCursorPos + 1);
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            currentCursorPos = (currentCursorPos <= 0 ? positions.Length - 1 : currentCursorPos - 1);
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (currentCursorPos)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    Application.Quit();
+                    Debug.Log("Game Exited");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        cursor.transform.position = new Vector3(cursor.transform.position.x, positions[currentCursorPos], cursor.transform.position.z);
+    }
+}
