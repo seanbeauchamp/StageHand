973ae171912962a927aa19fc76b4e402aa8caa4e
Sean Beauchamp <sean.marc.beauchamp@gmail.com>
2019-11-03 21:27:08 -0400

    Began implementing level select scene.

---
+++ Assets/Scripts/LevelSelect/LevelSelectManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectManager : MonoBehaviour
+{
+    public int numberOfLevels;
+    public GameObject levelBox;
+
+    private Vector2 firstBoxPos = new Vector2(32.4f, 115.9f);
+    private float offsetIncrement = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(levelBox.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
