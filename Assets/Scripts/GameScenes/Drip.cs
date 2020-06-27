197fa0595ee027bffec6ca7fee02052d9093e577
Sean Beauchamp <sean.marc.beauchamp@gmail.com>
2020-06-27 12:32:25 -0300

    Broken Vent/Drip Hazard now implemented.

---
+++ Assets/Scripts/GameScenes/Drip.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drip : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb2d;
+    private Collider2D collider;
+    private bool dropped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        collider = gameObject.GetComponent<Collider2D>();
+        StartCoroutine("StartDrop");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator StartDrop()
+    {
+        yield return new WaitForSeconds(1f);
+        animator.SetBool("DropStarted", true);
+        yield return new WaitForSeconds(.1f);
+        rb2d.gravityScale = 1.0f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameScenes/VentLeakScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VentLeakScript : MonoBehaviour
+{
+    [SerializeField] private GameObject drip;
+    [Range(2f, 10f)] [SerializeField] private float initialDrop = 2f;
+    [Range(4f, 10f)] [SerializeField] private float nextDrop = 4f;
+
+    private bool firstDropDripped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!UI.gameStarted || (!UI.gameRunning))
+            return;
+        if (!firstDropDripped)
+        {
+            Invoke("DropDrip", initialDrop);
+            firstDropDripped = true;
+        }
+    }
+
+    void DropDrip()
+    {
+        GameObject newDrip = Instantiate(drip, new Vector2(transform.position.x, transform.position.y - .15f), Quaternion.identity);
+        Invoke("DropDrip", nextDrop);
+    }
+}
